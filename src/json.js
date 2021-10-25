a48cd0071e587113ff976cb663ca5061a4d018bb
Igor Kowalczyk <49127376+IgorKowalczyk@users.noreply.github.com>
2021-10-25 11:02:24 +0000

    Remove not used code

--- src/json.js
+++ src/json.js
@@ -1,5 +1,3 @@
-const geoip = require("fast-geoip");
-
 function getRateLimitMessage() {
  const rate_limit_message = {
   ratelimit: true,
@@ -8,18 +6,4 @@ function getRateLimitMessage() {
  return rate_limit_message;
 }
 
-async function ipAddrInfo(ip) {
- if (ip.includes("::ffff:")) {
-  ip = ip.split(":").reverse()[0];
- }
- const lookedIP = await geoip.lookup(ip);
- if (ip === "127.0.0.1" || ip === "::1") {
-  return { error: "This won't work on localhost!" };
- }
- if (!lookedIP) {
-  return { error: "Error occured while trying to process the information!" };
- }
- return {ip, ...lookedIP};
-}
-
-module.exports = { getRateLimitMessage, ipAddrInfo };
+module.exports = { getRateLimitMessage };
