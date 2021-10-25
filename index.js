ee33d3c290cb08eded9d4d34001bc94e1a43faae
Igor Kowalczyk <49127376+IgorKowalczyk@users.noreply.github.com>
2021-10-25 10:33:50 +0000

    Update API

--- index.js
+++ index.js
@@ -39,10 +39,16 @@ app.get("/json", async function (req, res) {
 iplocate(ip).then(function(results) {
  const response = {
    ip: ip,
-   ...(req.query.city == "true" && {city: results.city}),
-   ...(req.query.country == "true" && {country: results.country}),
-   ...(req.query.country_code == "true" && {country_code: results.country_code}),
-   ...(req.query.continent == "true" && {continent: results.continent}),
+   ...((req.query.city == "true" || req.query.show_all == "true") && {city: results.city}),
+   ...((req.query.country == "true" || req.query.show_all == "true") && {country: results.country}),
+   ...((req.query.country_code == "true" || req.query.show_all == "true") && {country_code: results.country_code}),
+   ...((req.query.continent == "true" || req.query.show_all == "true") && {continent: results.continent}),
+   ...((req.query.latitude == "true" || req.query.show_all == "true") && {latitude: results.latitude}),
+   ...((req.query.longitude == "true" || req.query.show_all == "true") && {longitude: results.longitude}),
+   ...((req.query.time_zone == "true" || req.query.show_all == "true") && {time_zone: results.time_zone}),
+   ...((req.query.postal_code == "true" || req.query.show_all == "true") && {postal_code: results.postal_code}),
+   ...((req.query.org == "true" || req.query.show_all == "true") && {org: results.org}),
+   ...((req.query.asn == "true" || req.query.show_all == "true") && {asn: results.asn}),
  };
  res.send(response);
 });
